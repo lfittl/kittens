cfac98f4a61b0af3675c1876cd598ab3392b1a3f
Lukas Fittl <lukas@fittl.com>
2015-01-15 23:45:37 +0100

    Bugfixes.

--- db/migrations/001_create_events.rb
+++ db/migrations/001_create_events.rb
@@ -4,31 +4,34 @@ Sequel.migration do
   up do
     run <<-EOS
       CREATE TABLE postgres_events (
-        emitted_at TIMESTAMP WITH TIME ZONE,
-        proc_id TEXT,
-        message TEXT
+        "emitted_at" TIMESTAMP WITH TIME ZONE,
+        "proc_id" TEXT,
+        "message" TEXT
       );
 
       CREATE INDEX ON postgres_events(emitted_at);
 
       CREATE TABLE router_errors (
-        emitted_at TIMESTAMP WITH TIME ZONE,
-        code TEXT,
-        desc TEXT,
-        method TEXT,
-        path TEXT,
-        host TEXT,
-        request_id TEXT
-        fwd TEXT,
-        dyno TEXT,
-        connect TEXT,
-        service TEXT,
-        status INTEGER
+        "emitted_at" TIMESTAMP WITH TIME ZONE,
+        "code" TEXT,
+        "desc" TEXT,
+        "method" TEXT,
+        "path" TEXT,
+        "host" TEXT,
+        "request_id" TEXT,
+        "fwd" TEXT,
+        "dyno" TEXT,
+        "connect" TEXT,
+        "service" TEXT,
+        "status" INTEGER
       );
 
       CREATE INDEX ON router_errors(emitted_at);
     EOS
   end
 
-  down { run "DROP TABLE IF EXISTS events;" }
+  down do
+    run "DROP TABLE IF EXISTS postgres_events;"
+    run "DROP TABLE IF EXISTS router_errors;"
+  end
 end
